05c3aa0ceb4e503a5c3d23ca82111dfbfeaaa218
connorhsm <connorhsmith@gmail.com>
2021-12-04 16:48:06 +1100

    Add emoticon guide

---
+++ commands/emoticon.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('emoticon')
+		.setDescription('How to stop Discord from automatically changing your emoticons into emojis'),
+	async execute(interaction) {
+		return interaction.reply('1. You must be on PC to change this setting.\n2. Go to user settings (the gear in the bottom left)\n3. Find the \'Text & Images\' tab\n4. Deselect the setting \'Automatically convert emoticons in your messages to emoji\'\n\n\:\)');
+	},
+};
