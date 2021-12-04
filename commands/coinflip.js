dc65ad8d9b7e45760f2ae404edfd707caaa66c89
connorhsm <connorhsmith@gmail.com>
2021-12-04 17:03:18 +1100

    Update coinflip

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -5,7 +5,7 @@ module.exports = {
 		.setName('coinflip')
 		.setDescription('Flip a coin'),
 	async execute(interaction) {
-		const coin = Math.floor(Math.random())
+		const coin = Math.floor(Math.random());
 		return interaction.reply(coin ? 'Heads' : 'Tails');
 	},
 };
