4c42d45cffd4ea155898865b430b7880cc13705d
connorhsm <connorhsmith@gmail.com>
2021-12-04 17:03:30 +1100

    Add dye command

---
+++ commands/dye.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('dye')
+		.setDescription('Let me suggest a colour for you'),
+	async execute(interaction) {
+		const dyes = ["I like red!", "Green is the best!", "Blue would be nice!", "What about black!", "White?", "Maybe yellow?"];
+		return interaction.reply(Math.floor(Math.random() * dyes.length));
+	},
+};
