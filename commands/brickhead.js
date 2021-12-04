e29208c024931337741dcc3ea37290f0fb382512
connorhsm <connorhsmith@gmail.com>
2021-12-04 17:07:49 +1100

    brickhead command

---
+++ commands/brickhead.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('brickhead')
+		.setDescription('brickhead?'),
+	async execute(interaction) {
+		return interaction.reply('Dragon is the brick lord!');
+	},
+};
