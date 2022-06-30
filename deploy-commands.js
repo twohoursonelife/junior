2591b4eba13fec3db8503a42deca23579f7200bb
connorhsm <connorhsmith@gmail.com>
2022-06-30 15:45:07 +1000

    Deploy commands script now checks that necessary env variables are set first

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,13 +2,21 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const fs = require('fs');
 
+const clientID = process.env.CLIENTID
+const guildID = process.env.GUILDID
+
+if (typeof clientID == 'undefined' || typeof guildID == 'undefined' ){
+	console.log('CLIENTID and GUILDID environment vairables must be set to deploy commands.');
+	process.exit();
+};
+
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
-}
+};
 
 const rest = new REST({ version: '9' }).setToken(process.env.DEV_TOKEN ? process.env.DEV_TOKEN : process.env.PROD_TOKEN);
 
@@ -17,7 +25,7 @@ const rest = new REST({ version: '9' }).setToken(process.env.DEV_TOKEN ? process
 		console.log('Started refreshing application (/) commands.');
 
 		await rest.put(
-			Routes.applicationGuildCommands(process.env.CLIENTID, process.env.GUILDID),
+			Routes.applicationGuildCommands(clientID, guildID),
 			{ body: commands },
 		);
 
