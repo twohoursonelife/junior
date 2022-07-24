548831ac3ac362b5eb71a56f1162d3f1deabf263
connorhsm <connorhsmith@gmail.com>
2022-07-24 22:05:57 +1000

    Update to new intents format

--- junior.js
+++ junior.js
@@ -1,9 +1,9 @@
 // Thanks to https://github.com/discordjs/guide for base code samples.
 
 const fs = require('fs');
-const { Client, Collection, Intents } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
