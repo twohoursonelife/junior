e763afd81e14021e3e4e4e5e8844141793720963
Connor Smith <connorhsmith@gmail.com>
2022-09-19 12:23:00 +1000

    Refactor deploy command script. Simpler logic of dev or prod mode.

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,25 +4,23 @@ const fs = require('fs');
 
 let clientID;
 let guildID;
+let botToken;
 
-if (process.env.CLIENTID) {
-	clientID = process.env.CLIENTID
+if (process.env.DEV_TOKEN) {
+	botToken = process.env.DEV_TOKEN;
+	clientID = "912705407850532904";
+	guildID = "678098930503909386";
+
+
+} else if (process.env.PROD_TOKEN) {
+	botToken = process.env.PROD_TOKEN;
+	clientID = process.env.CLIENTID;
+	guildID = process.env.GUILDID;
+
 } else {
-	clientID = "912705407850532904"; // Dev client
-	console.log('CLIENTID environment variable not set, defaulting to dev client.');
-};
-
-if (process.env.GUILDID) {
-	guildID = process.env.GUILDID
-} else {
-	guildID = "678098930503909386"; // Dev guild
-	console.log('GUILDID environment variable not set, defaulting to dev guild.');
-};
-
-if (!process.env.DEV_TOKEN && !process.env.PROD_TOKEN) {
-	console.log("DEV_TOKEN and PROD_TOKEN were not set when deploying commands! Exiting.");
+	console.log("DEV_TOKEN or PROD_TOKEN were not set when deploying commands! Exiting.");
 	process.exit();
-}
+};
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -32,7 +30,7 @@ for (const file of commandFiles) {
 	commands.push(command.data.toJSON());
 };
 
-const rest = new REST({ version: '10' }).setToken(process.env.DEV_TOKEN ? process.env.DEV_TOKEN : process.env.PROD_TOKEN);
+const rest = new REST({ version: '10' }).setToken(botToken);
 
 (async () => {
 	try {
