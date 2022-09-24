785be746cfd56e3a0fe068ddd2e120daa094be6b
connorhsm <connorhsmith@gmail.com>
2022-09-25 01:36:20 +1000

    General spawn code rename

--- commands/videos.js
+++ commands/videos.js
@@ -6,7 +6,7 @@ module.exports = {
 		.setDescription('Helpful video links'),
 	async execute(interaction) {
 		return interaction.reply({
-			content: 'Here are some great helpful videos to watch to get you started on your 2HOL journey:\n\nHow to make a seed and survive:\n`https://youtu.be/WsaB9XiQNjQ`\n\nThings to do as a new player:\n`https://youtu.be/nUI9gzhA7LE`\n\nYum / eating smartly:\n`https://youtu.be/wncC_KI6u_o`\n\nHunting:\n`https://youtu.be/IJZ4IOlGFvo`\n\nMining:\n`https://youtu.be/1KchOC45zrc`\n\nBronze / basic smithing:\n`https://youtu.be/IsnUn00U058`',
+			content: 'Here are some great helpful videos to watch to get you started on your 2HOL journey:\n\nHow to make a spawn code and survive:\n`https://youtu.be/WsaB9XiQNjQ`\n\nThings to do as a new player:\n`https://youtu.be/nUI9gzhA7LE`\n\nYum / eating smartly:\n`https://youtu.be/wncC_KI6u_o`\n\nHunting:\n`https://youtu.be/IJZ4IOlGFvo`\n\nMining:\n`https://youtu.be/1KchOC45zrc`\n\nBronze / basic smithing:\n`https://youtu.be/IsnUn00U058`',
 		});
 	},
 };
