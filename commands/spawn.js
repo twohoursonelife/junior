8af8a6ffe252626f0bc211bfa20628ac5c008881
connorhsm <connorhsmith@gmail.com>
2022-09-25 01:24:30 +1000

    Update spawn command for spawn codes and family spawns

--- commands/spawn.js
+++ commands/spawn.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('spawn')
 		.setDescription('Guide about spawning'),
 	async execute(interaction) {
-		return interaction.reply('What happens when you spawn into the world?\n\n**Using a seed:** You will spawn as an eve wherever that seed dropped you. You can have babies there.\n**Without a seed:** If there **is** a fertile female in the world, you will spawn as a baby at their feet. If there **is not** a fertile female in the world, you will spawn as an Eve in the center of the map. There is a stone with "unless" written on it, to mark the center. The area to the east is called Public Town (PT).');
+		return interaction.reply('What happens when you spawn into the world?\n\n**Playing with friends?**\nIf playing with friends, follow the "twinning" guide. "No" is the default option.\n\n**Specific spawn:**\n**- Family name:** Enter the desired family surname. You will spawn as a baby to an eligible fertile female of that family. You will get an error if there is no family by that name or eligible mother.\n**- Spawn code:** Enter the desired spawn code. You will spawn as an Eve wherever that spawn code takes you.\n\n**Random spawn:** This is the default choice. If there is an eligible fertile female in the world, you will spawn as their baby. If there is not a fertile female in the world, you will spawn as an Eve in the center of the map, or at one or more predetermined spawn codes, depending on server settings at the time.');
 	},
 };
