78ada20f73dbf033e5fbe52c54e001a09b85140f
connorhsm <connorhsmith@gmail.com>
2022-10-11 01:33:51 +1100

    Hide link embed for notavirus command

--- commands/notavirus.js
+++ commands/notavirus.js
@@ -6,7 +6,7 @@ module.exports = {
 		.setDescription('Quick info that the game is not a virus'),
 	async execute(interaction) {
 		return interaction.reply({
-			content: 'Rest assured, the game is not a virus.\n2HOL is a relatively small project, some antivirus programs are simply showing false positives.\nWe\'re working hard to fix this in future thanks to kind donations from our community, allowing us to purchase certificates to authenticate with Microsoft.\n\nPlease see that we have 17,000+ members in this Discord as well as many who have hundreds, if not thousands of hours online at any time. https://discord.gg/Jd9Es3f\nThe project is also open source, allowing code to be verified here https://github.com/twohoursonelife/OneLife',
+			content: 'Rest assured, the game is not a virus.\n2HOL is a relatively small project, some antivirus programs are simply showing false positives.\nWe\'re working hard to fix this in future thanks to kind donations from our community, allowing us to purchase certificates to authenticate with Microsoft.\n\nPlease see that we have 17,000+ members in this Discord as well as many who have hundreds, if not thousands of hours online at any time. https://discord.gg/Jd9Es3f\nThe project is also open source, allowing code to be verified here <https://github.com/twohoursonelife/OneLife>',
 		});
 	},
 };
