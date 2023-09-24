b7f201a595ec6b3ead28d4b373c035c866e85503
Connor Smith <connorhsmith@gmail.com>
2023-09-24 17:06:54 +1000

    Correct mention format

--- commands/birthday.js
+++ commands/birthday.js
@@ -6,7 +6,7 @@ module.exports = {
 		.setDescription('My Birthday!'),
 	async execute(interaction) {
 		return interaction.reply({
-			content: 'My birthday is September 18th! I am 2 years old! I\'d like to thank everyone who came to my party! Shoutout to @<313791022121615361> for winning the most points at my recent party!',
+			content: 'My birthday is September 18th! I am 2 years old! I\'d like to thank everyone who came to my party! Shoutout to <@313791022121615361> for winning the most points at my recent party!',
 		});
 	},
 };
