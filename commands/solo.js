ef5b90788fd97be266f26d31966f898e5f6087e3
Connor Smith <connorhsmith@gmail.com>
2024-01-21 23:02:19 +1100

    Update linking

--- commands/solo.js
+++ commands/solo.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('solo')
 		.setDescription('How to play 2HOL solo'),
 	async execute(interaction) {
-		return interaction.reply('How to play solo or as a single player\n\nUsing a spawn code is the easiest way to play alone. While you will be on the same map as other players, a spawn code allows you to find an isolated space to play.\n\nSay \"no bb\" in game each life to become infertile. If you decide you\'d like to have children later \"yes bb\" will make you fertile again.\n\nUse /spawncodes to find out more.\n\nIf you want a completely isolated experience you will need to set up your own server, this is a technically advanced process https://github.com/risvh/miniOneLifeCompile');
+		return interaction.reply('How to play solo or as a single player\n\nUsing a spawn code is the easiest way to play alone. While you will be on the same map as other players, a spawn code allows you to find an isolated space to play. **Say \"no bb\"** in game each life to become infertile. If you decide you\'d like to have children later \"yes bb\" will make you fertile again.\n\nUse /spawncodes to find out more.\n\nIf you want a completely isolated experience you will need to [set up your own server](<https://github.com/risvh/miniOneLifeCompile>), this is a technically advanced process.');
 	},
 };
