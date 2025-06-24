aa4cbabc9dd835b388b483fd28264acf6d983f4f
TanyaPegasus <thesilverpegasus@gmail.com>
2025-06-24 23:33:03 +1000

    Update screenshots command

--- commands/screenshot.js
+++ commands/screenshot.js
@@ -6,7 +6,7 @@ module.exports = {
 		.setDescription('How to screenshot 2HOL'),
 	async execute(interaction) {
 		return interaction.reply({
-			content: 'Press: alt + enter to go into windowed mode\nPress: alt + print screen to take a screengrab\nPaste into Paint 3D or equivalent.\nCrop as needed.\n\nTo get back out of windowed mode, press alt + enter again.',
+			content: '**Screenshots**\nPress `=` while in game to take a screenshot in .png format. You\'ll find it inside the "Screenshots" folder inside your 2HOL game folder.\nIf your game is in windowed or borderless modes, other default methods will also work, such as Windows snipping tool *(Windows + Shift + S).*\n\nYou can temporarily hide the UI by holding down the grave key ( **\`** ), usually found on the same key as `~`.',
 		});
 	},
 };
