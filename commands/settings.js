b0c3509067026b3e71d8e3691bb438b4ad283d0f
Connor Smith <connorhsmith@gmail.com>
2025-08-15 01:06:51 +1000

    Command wording adjustments

--- commands/settings.js
+++ commands/settings.js
@@ -6,7 +6,7 @@ module.exports = {
 		.setDescription('2HOL settings information'),
 	async execute(interaction) {
 		return interaction.reply({
-			content: '**Settings**\nEnable "Zoom" in the `Gameplay` tab, then zoom using your scroll wheel while you play.\n\n"Keyboard actions", such as movement with WASD, can be enabled in the `Control` tab.\n This is also where you\'ll find settings for "Fullscreen" and "Borderless". Your game will need to be restarted to apply these changes.\n\nTo display information about your current life in your discord profile, go to the `Discord` tab.\n\nThe `Advanced` tab is where you\'ll find a lot of the old modded \'Hetuw\' features, like "Show Coordinates", as well as your list of "Command Shortcuts" - For more on those, use /shortcuts\n"Save Map Files" is also found under advanced. Only enable that one if you\'re using Town Planner.',
+			content: '**Settings**\nEnable "Zoom" in the `Gameplay` tab, then zoom using your scroll wheel while you play.\n\n"Keyboard actions", such as movement with WASD, can be enabled in the `Control` tab.\n This is also where you\'ll find settings for "Fullscreen" and "Borderless".\n\nTo adjust display information about your current life in your discord profile, go to the `Discord` tab.\n\nThe `Advanced` tab is where you\'ll find other complex quality of life features like "Show Coordinates", as well as your list of "Command Shortcuts" - For more on those, use /shortcuts.\n\n"Save Map Files" is also found under advanced. Only enable that one if you\'re using Town Planner.',
 		});
 	},
 };
